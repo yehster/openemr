417b33801a5a3afd303bccd8ecaf7ff31ae72917
Kevin Yeh <kevinyeh@alum.mit.edu>
2013-03-06 21:19:45 -0500

    insurance info in encounter report

--- interface/main/left_nav.php
+++ interface/main/left_nav.php
@@ -1306,7 +1306,7 @@ if (!empty($reg)) {
       <li><a class="collapsed_lv2"><span><?php xl('Visits','e') ?></span></a>
         <ul>
           <?php if (!$GLOBALS['disable_calendar']) genMiscLink('RTop','rep','0',xl('Appointments'),'reports/appointments_report.php'); ?>
-          <?php  genMiscLink('RTop','rep','0',xl('Encounters'),'reports/encounters_report.php'); ?>
+          <?php  genMiscLink('RTop','rep','0',xl('Encounters'),'reports/encounters_report_insurance.php'); ?>
           <?php if (!$GLOBALS['disable_calendar']) genMiscLink('RTop','rep','0',xl('Appt-Enc'),'reports/appt_encounter_report.php'); ?>
 <?php if (empty($GLOBALS['code_types']['IPPF'])) { ?>
           <?php genMiscLink('RTop','rep','0',xl('Superbill'),'reports/custom_report_range.php'); ?>
