49d17d2f0350636db115365b764e73bcb70bed51
Kevin Yeh <kevinyeh@alum.mit.edu>
2013-05-04 09:02:22 -0400

    url fix

--- interface/main/menu/js/menu_view_model.js
+++ interface/main/menu/js/menu_view_model.js
@@ -15,7 +15,7 @@ function menu_click(data,evt)
     //top.displayInFrame(this.target,"",this.url);
     if(this.type==="LoadFrame")
     {
-        view_model.tabStates()[1].src("/openemr/interface/"+data.url);
+        view_model.tabStates()[1].src(pathWebroot+data.url);
         set_visible(1,false);
     }
     return true;
