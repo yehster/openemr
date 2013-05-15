622332a7f669a04a575ff0a9d56abdc1f7c105bf
Kevin Yeh <kevinyeh@alum.mit.edu>
2013-05-15 03:23:02 -0400

    encounter tweaks

--- interface/main/tabs/main_title_updates.js
+++ interface/main/tabs/main_title_updates.js
@@ -16,15 +16,5 @@ else if(rawdata=='Past Encounter List')
 top.window.parent.left_nav.loadFrame2('pel1','RBot','patient_file/history/encounters.php')
 return true;
 }
-    var parts = rawdata.split("~");
-    var enc = parts[0];
-    var datestr = parts[1];
-    var f = top.window.parent.left_nav.document.forms[0];
-frame = 'RBot';
-    if (!f.cb_bot.checked) frame = 'RTop'; else if (!f.cb_top.checked) frame = 'RBot';
-
-    top.restoreSession();
-    parent.left_nav.setEncounter(datestr, enc, frame);
-    parent.left_nav.setRadio(frame, 'enc');
-    top.displayInFrame(frame, '../patient_file/encounter/encounter_top.php?set_encounter=' + enc);
+    top.set_encounter(rawdata);
 }
