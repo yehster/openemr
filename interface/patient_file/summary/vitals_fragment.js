1cf3451557ea835fc1d4a75624453a4b9c8920eb
Kevin Yeh <kevinyeh@alum.mit.edu>
2013-08-16 10:53:52 -0400

    direct growth chart link + font fix

---
+++ interface/patient_file/summary/vitals_fragment.js
@@ -0,0 +1,20 @@
+function displayChart()
+{
+    var hidden_vitals=$("#hidden_vitals");
+    var vitals_document=hidden_vitals.get(0).contentDocument;    
+    $("#pdfchart",vitals_document).click();
+}
+function showGrowthChart()
+{
+    var hidden_vitals=$("#hidden_vitals");
+    if(hidden_vitals.length==0)
+    {
+        hidden_vitals=$("<iframe style='display:none' onload='displayChart()' id='hidden_vitals' src='../encounter/trend_form.php?formname=vitals'></iframe)");
+        hidden_vitals.appendTo($('body'));
+    }
+    else
+    {
+        var vitals_document=hidden_vitals.get(0);
+        vitals_document.src=vitals_document.src;
+    }
+}
--- interface/patient_file/summary/vitals_fragment.php
+++ interface/patient_file/summary/vitals_fragment.php
@@ -52,6 +52,9 @@ if ( !$result ) //If there are no disclosures recorded
   call_user_func("vitals_report", '', '', 2, $result['id']);
   ?>  <span class='text'>
   <br />
+  <script src='vitals_fragment.js' type='text/javascript'/></script>
+  <a onclick='showGrowthChart()'><?php echo xlt('Show Growth Chart')?></a>
+  <br>
   <a href='../encounter/trend_form.php?formname=vitals' onclick='top.restoreSession()'><?php echo htmlspecialchars(xl('Click here to view and graph all vitals.'),ENT_NOQUOTES);?></a>
   </span><?php
 } ?>
