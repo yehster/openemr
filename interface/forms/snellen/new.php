4a997ce8d9590c0128a7d4267233e826be495b3c
Kevin Yeh <kevinyeh@alum.mit.edu>
2013-09-13 16:38:59 -0400

    Optometry Referral

--- interface/forms/snellen/new.php
+++ interface/forms/snellen/new.php
@@ -11,4 +11,19 @@ require ("C_FormSnellen.class.php");
 
 $c = new C_FormSnellen();
 echo $c->default_action();
+
+
 ?>
+<script type="text/javascript" src="<?php echo $GLOBALS['webroot'] ?>/library/js/jquery-2.0.3.min.js"></script>
+<script type="text/javascript">
+    $("#optometry_ref").click( function()
+        {
+            var notes=$("textarea[name='notes']");
+            var phrase="Referred to optometry.";
+            if(notes.val().indexOf(phrase)===-1)
+                {
+                    var text=notes.val()+phrase;
+                    notes.val(text);    
+                }
+        });
+</script>
