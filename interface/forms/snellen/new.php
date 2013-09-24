9a8f3ab50be90d59750f0b3ec2805f83b14ec53f
Kevin Yeh <kevinyeh@alum.mit.edu>
2013-09-24 15:23:24 -0400

    bug fix for referral button, only on new

--- interface/forms/snellen/new.php
+++ interface/forms/snellen/new.php
@@ -14,16 +14,3 @@ echo $c->default_action();
 
 
 ?>
-<script type="text/javascript" src="<?php echo $GLOBALS['webroot'] ?>/library/js/jquery-2.0.3.min.js"></script>
-<script type="text/javascript">
-    $("#optometry_ref").click( function()
-        {
-            var notes=$("textarea[name='notes']");
-            var phrase="Referred to optometry.";
-            if(notes.val().indexOf(phrase)===-1)
-                {
-                    var text=notes.val()+phrase;
-                    notes.val(text);    
-                }
-        });
-</script>
