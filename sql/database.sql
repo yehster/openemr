eaa232884fa8c17fb0d2201ee8d4cd1fa504f9a2
Kevin Yeh <kevinyeh@alum.mit.edu>
2013-12-04 00:16:17 -0500

    rule_action_item table definition for strict SQL

--- sql/database.sql
+++ sql/database.sql
@@ -4542,7 +4542,7 @@ CREATE TABLE `rule_action_item` (
   `category` varchar(31) NOT NULL DEFAULT '' COMMENT 'Maps to list_options list rule_action_category',
   `item` varchar(31) NOT NULL DEFAULT '' COMMENT 'Maps to list_options list rule_action',
   `clin_rem_link` varchar(255) NOT NULL DEFAULT '' COMMENT 'Custom html link in clinical reminder widget',
-  `reminder_message` text  NOT NULL DEFAULT '' COMMENT 'Custom message in patient reminder',
+  `reminder_message` text COMMENT 'Custom message in patient reminder',
   `custom_flag` tinyint(1) NOT NULL default 0 COMMENT '1 indexed to rule_patient_data, 0 indexed within main schema',
   PRIMARY KEY  (`category`,`item`)
 ) ENGINE=MyISAM ;
