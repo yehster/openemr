7dd1c81f5df775a73a768655ad350ea6b781e93a
Kevin Yeh <kevinyeh@alum.mit.edu>
2014-01-31 16:58:09 -0500

    ts fix

--- interface/forms/infant_sick_visit/infant_sick_common.php
+++ interface/forms/infant_sick_visit/infant_sick_common.php
@@ -1,6 +1,6 @@
 <?php require_once($webserver_root."/interface/forms_knockout/base_document_common.php"); ?>
 
-<script type="text/javascript" src="<?php echo $GLOBALS['webroot'] ?>/interface/forms/infant_sick_visit/js/infant_sick_visit_metadata.js?<?php echo (new DateTime())->getTimestamp();?>"></script>
+<script type="text/javascript" src="<?php echo $GLOBALS['webroot'] ?>/interface/forms/infant_sick_visit/js/infant_sick_visit_metadata.js?<?php $ts = new DateTime(); echo $ts->getTimestamp();?>"></script>
 <script>
     var view_model=new infant_sick_visit_document();
     var uuid='<?php echo $form_data['uuid']?>';
