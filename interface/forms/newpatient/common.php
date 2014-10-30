3a6bb4835e122ade1e3830816dbd132566d4d5f2
Kevin Yeh <kevinyeh@alum.mit.edu>
2014-10-30 15:42:51 -0400

    For New Encounter, choose visit category based on visit history, add problem text to consultation reason when issue select.

--- interface/forms/newpatient/common.php
+++ interface/forms/newpatient/common.php
@@ -70,6 +70,18 @@ $ires = sqlStatement("SELECT id, type, title, begdate FROM lists WHERE " .
 <?php include_once("{$GLOBALS['srcdir']}/dynarch_calendar_en.inc.php"); ?>
 <script type="text/javascript" src="<?php echo $GLOBALS['webroot'] ?>/library/dynarch_calendar_setup.js"></script>
 <?php include_once("{$GLOBALS['srcdir']}/ajax/facility_ajax_jav.inc.php"); ?>
+<script type='text/javascript' charset='utf-8'>
+jQuery(document).ready(function(){
+  jQuery('select[name^="issues"] option').click(function(){
+    var id = jQuery(this).val();
+    jQuery.getJSON("issue_from_id.php",{id:id},function(data){
+      //we only want to append a space if there is already text in the textbox
+      if (data["Error"] === undefined)
+        jQuery('textarea[name="reason"]').text(jQuery('textarea[name="reason"]').text()+(jQuery('textarea[name="reason"]').text()==""?"":" ")+data['title']);
+    });
+  });
+});
+</script>
 <script language="JavaScript">
 
  var mypcc = '<?php echo $GLOBALS['phone_country_code'] ?>';
@@ -188,13 +200,36 @@ function cancelClicked() {
       <select name='pc_catid' id='pc_catid'>
 	<option value='_blank'>-- <?php echo xlt('Select One'); ?> --</option>
 <?php
+/*
+  If we're viewing a previous record, we automatically select the option
+  chosen for that record
+  If we're creating a new record, we want to select an option automatically
+  depending on the patient's previous visit. If they are a new patient or have
+  not visited in more than 6 months, set the visit category to "New Patient".
+  Otherwise, set to "Established Patient"
+  
+  established patient id=9, new patient id = 10
+*/
+  $recent_encounters = sqlStatement("select id from form_encounter where pid=$pid and DATEDIFF(CURDATE(),date) < 183");
+  $isEstablishedPatient = (sqlNumRows($recent_encounters) > 0);
  $cres = sqlStatement("SELECT pc_catid, pc_catname " .
   "FROM openemr_postcalendar_categories ORDER BY pc_catname");
  while ($crow = sqlFetchArray($cres)) {
   $catid = $crow['pc_catid'];
   if ($catid < 9 && $catid != 5) continue;
   echo "       <option value='" . attr($catid) . "'";
-  if ($viewmode && $crow['pc_catid'] == $result['pc_catid']) echo " selected";
+  if ($viewmode && $crow['pc_catid'] == $result['pc_catid'])
+  {
+    echo " selected";
+  }
+  else if (!$viewmode && $isEstablishedPatient && $crow['pc_catid'] == 9)
+  {
+    echo " selected";
+  }
+  else if(!$viewmode && !$isEstablishedPatient && $crow['pc_catid'] == 10)
+  {
+    echo " selected";
+  }
   echo ">" . text(xl_appt_category($crow['pc_catname'])) . "</option>\n";
  }
 ?>
