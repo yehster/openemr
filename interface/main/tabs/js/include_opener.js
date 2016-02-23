63af3b43569895bc0d8c0921112b7e8c78741b1d
Kevin Yeh <kevinyeh@alum.mit.edu>
2016-02-23 01:39:06 -0500

    Close button on dlg boxes

--- interface/main/tabs/js/include_opener.js
+++ interface/main/tabs/js/include_opener.js
@@ -22,10 +22,13 @@ if(top.tab_mode===true)
             function()
             {
                 var dialogDiv=top.$("#dialogDiv");
+                var frameName=window.name
                 var body=top.$("body");
-                    var removeFrame=body.find("iframe.dialogIframe[name='"+window.name+"']");
+                    var removeFrame=body.find("iframe[name='"+frameName+"']");
                     removeFrame.remove();
-                    if(body.children("iframe.dialogIframe").length===0)
+                    var removeDiv=body.find("div.dialogIframe[name='"+frameName+"']");
+                    removeDiv.remove();
+                    if(body.children("div.dialogIframe").length===0)
                     {   
                         dialogDiv.hide();
                     };
